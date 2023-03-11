1a0060557c52d8aa9ec4982f2da4a30fff233700
ArturPawelski <arturpawelski13@gmail.com>
2023-03-11 23:10:12 +0100

    third box

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -32,33 +32,34 @@ const Home = () => {
             <div className='flex flex-col justify-center items-center max-w-[90%] mx-auto mt-[4rem] gap-4 shadow-2xl py-8 rounded-2xl px-4 bg-[#000B70] bg-opacity-60 md:flex-row-reverse md:bg-transparent md:max-w-[80%] md:mt-[8rem]'>
 
                 <div className=' md:w-[50%]'>
-                    <img src={Food} alt="" className=' w-[40%] sm:w-[37%] md:w-[70%] mx-auto ' />
+                    <img src={Food} alt="" className=' w-[40%] sm:w-[37%] md:w-[70%] mx-auto md:mr-[20%] ' />
                 </div>
 
-                <div className=' md:w-[50%] '>
+                <div className=' md:w-[50%]  '>
 
-                    <h2 className=' text-center   text-white font-lol font-bold text-2xl mt-4 md:text-center md:leading-[2.6rem] md:text-3xl lg:leading-[2.8rem] xl:text-4xl xl:leading-[3rem] 2xl:text-5xl 2xl:leading-[4rem] '>KORZYSTAJ Z PODANYCH PRZEPISÓW</h2>
-                    <p className='text-center  text-white font-lol font-light text-[1rem] mt-4 md:text-center lg:text-2xl xl:mt-8 xl:text-[1.7rem] xl:leading-[2.5rem] 2xl:text-[2rem] 2xl:leading-[2.8rem] '> i na ich podstawie <br /> dodawaj produkty do listy</p>
+                    <h2 className=' text-center   text-white font-lol font-bold text-2xl mt-4 md:text-left md:ml-[30%]  md:ml- md:leading-[2.6rem] md:text-3xl lg:leading-[2.8rem] xl:text-4xl xl:leading-[3rem] 2xl:text-5xl 2xl:leading-[4rem] '>KORZYSTAJ Z PODANYCH PRZEPISÓW</h2>
+                    <p className='text-center  text-white font-lol font-light text-[1rem] mt-4 md:text-left md:ml-[30%] lg:text-2xl xl:mt-8 xl:text-[1.7rem] xl:leading-[2.5rem] 2xl:text-[2rem] 2xl:leading-[2.8rem] '> i na ich podstawie <br /> dodawaj produkty do listy</p>
 
                 </div>
             </div >
 
 
 
+            <div className='flex flex-col justify-center items-center max-w-[90%] mx-auto mt-[4rem] gap-4 shadow-2xl py-8 rounded-2xl px-4 bg-[#000B70] bg-opacity-60 md:flex-row  md:bg-transparent md:max-w-[80%] md:mt-[8rem] '>
 
+                <div className=' md:w-[50%] '>
+                    <img src={Phone3} alt="" className=' w-[30%] relative left-6 sm:w-[28%] md:w-[45%] mx-auto ' />
+                </div>
 
+                <div className=' md:w-[50%] '>
 
-            <div className='flex flex-col justify-center items-center max-w-[90%] mx-auto mt-[4rem] gap-4 shadow-2xl py-8 rounded-2xl px-4 bg-[#000B70] bg-opacity-60'>
-
-                <img src={Phone3} alt="" className=' w-[40%] relative left-3  ' />
-
-                <div className=''>
-
-                    <h2 className=' text-center text-white font-lol font-bold text-2xl mt-4 '>TWÓRZ SWOJE WŁASNE <br /> LISTY ZAKUPÓW </h2>
-                    <p className='text-center text-white font-lol font-light text-[1rem] mt-4'>i korzystaj z przykładowych list z <br /> “SKLEP LISTA” wszystko jest proste</p>
+                    <h2 className=' text-center   text-white font-lol font-bold text-2xl mt-4 md:text-left md:leading-[2.6rem] md:text-3xl lg:leading-[2.8rem] xl:text-4xl xl:leading-[3rem] 2xl:text-5xl 2xl:leading-[4rem] '> DODAWAJ WŁASNE PRZEPISY</h2>
+                    <p className='text-center  text-white font-lol font-light text-[1rem] mt-4 md:text-left lg:text-2xl xl:mt-8 xl:text-[1.7rem] xl:leading-[2.5rem] 2xl:text-[2rem] 2xl:leading-[2.8rem] '></p>
 
                 </div>
-            </div>
+            </div >
+
+
 
 
             <div className='flex flex-col justify-center items-center max-w-[90%] mx-auto mt-[4rem] mb-[4rem] gap-4 shadow-2xl py-8 rounded-xl px-4 bg-[#000B70] bg-opacity-60'>
