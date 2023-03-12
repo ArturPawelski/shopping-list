2202e0799af7de902d8c05a007bb146a2200cfae
ArturPawelski <arturpawelski13@gmail.com>
2023-03-12 15:17:27 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import Logo from '../images/SKLEP-LISTA-footer.png'
+import { HiOutlineMail } from 'react-icons/hi'
+import { MdOutlinePhoneIphone } from 'react-icons/md'
+import { AiFillFacebook } from 'react-icons/ai'
+import { BsInstagram } from 'react-icons/bs'
+import { SiTiktok } from 'react-icons/si'
+import { VscTwitter } from 'react-icons/vsc'
 
 const Footer = () => {
 
 
     return (
-        <div>
-
-
+        <div className='w-full font-lol bg-[#FFFFFF] bg-opacity-[85%]'>
+            <img src={Logo} alt="" />
+            <HiOutlineMail />
+            <MdOutlinePhoneIphone />
+            <AiFillFacebook />
+            <BsInstagram />
+            <SiTiktok />
+            <VscTwitter />
             footer
         </div>
     )
