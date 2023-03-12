69b29d1f2a2e19a936b3c5765649a25b9fac0b56
ArturPawelski <arturpawelski13@gmail.com>
2023-03-12 16:14:06 +0100

    mobile footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,15 +11,30 @@ const Footer = () => {
 
 
     return (
-        <div className='w-full font-lol bg-[#FFFFFF] bg-opacity-[85%]'>
-            <img src={Logo} alt="" />
-            <HiOutlineMail />
-            <MdOutlinePhoneIphone />
-            <AiFillFacebook />
-            <BsInstagram />
-            <SiTiktok />
-            <VscTwitter />
-            footer
+        <div className='w-full font-lol font-bold bg-[#FFFFFF] bg-opacity-[85%] mt-[8rem]   '>
+
+
+            <footer className='w-[90%] mx-auto py-12'>
+
+                <div className='w-full'>
+                    <img src={Logo} alt="/" className='w-[65%] mx-auto' />
+                </div>
+
+                <ul className='grid place-items-center mt-8'>
+                    <li className='flex items-center gap-2'><HiOutlineMail size={25} /> sklep.lista@gmail.com</li>
+                    <li className='flex items-center mt-4 gap-2'> <MdOutlinePhoneIphone size={25} /> 666 666 666 </li>
+                </ul>
+
+                <div className='flex justify-center items-center gap-[15%] mt-8'>
+                    <AiFillFacebook size={25} />
+                    <BsInstagram size={25} />
+                    <SiTiktok size={25} />
+                    <VscTwitter size={25} />
+                </div>
+
+            </footer>
+
+
         </div>
     )
 }
