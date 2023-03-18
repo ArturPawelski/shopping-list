4be34dacad721eeb06dbedd8f76bba33fa79a1c2
ArturPawelski <arturpawelski13@gmail.com>
2023-03-18 14:05:32 +0100

    twoja lista box

--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -1,45 +1,60 @@
 import React from 'react'
 import { BiCaretDown } from 'react-icons/bi'
+import TwojaLista from '../images/TWOJA.png'
 
 const Lista = () => {
+
+
+
+
+
     return (
         <div className='w-full font-lol'>
 
+            <section>
+
+                <button button className='w-[85%] shadow-[6px_4px_8px_rgba(0,0,0,0.50)] gap-4 flex justify-center items-center font-extrabold mx-auto mt-[6rem] py-2 px-8 bg-white rounded-xl sm:max-w-[500px]   ' > WYBIERZ PRODUKTY Z PODANYCH < BiCaretDown size={25} className='' /> </button >
 
 
-            <button className='w-[85%] shadow-[6px_4px_8px_rgba(0,0,0,0.50)] gap-4 flex justify-center items-center font-extrabold mx-auto mt-[6rem] py-2 px-8 bg-white rounded-xl sm:max-w-[500px]   '>WYBIERZ PRODUKTY Z PODANYCH <BiCaretDown size={25} className='' /> </button>
+                <div className=' w-full  mt-[6rem] flex flex-col bg-[#32388F] bg-opacity-90 py-8 px-8 mx-auto md:max-w-[650px]  md:rounded-2xl    '>
 
+                    <div className='flex flex-col md:flex-row md:items-center md:justify-center md:mx-auto  md:gap-[8rem]'>
 
+                        <div className='mx-auto grid '>
 
-
-            <div className=' w-full  mt-[6rem] flex flex-col bg-[#32388F] bg-opacity-90 py-8 px-8 mx-auto md:max-w-[650px]  md:rounded-2xl    '>
-
-                <div className='flex flex-col md:flex-row md:items-center md:justify-center md:mx-auto  md:gap-[8rem]'>
-
-                    <div className='mx-auto grid '>
-
-                        <p className='text-white font-bold mx-auto md:mx-0 '>WPISZ PRODUKT:</p>
-                        <input className='py-4 px-4 max-w-[18rem] rounded-lg  mx-auto shadow-[6px_4px_8px_rgba(0,0,0,0.35)]  ' type="text" placeholder='mleko...' />
-                    </div>
-
-                    <div className='flex justify-center gap-[4rem] mt-[2rem] md:mt-0'>
-                        <div >
-                            <p className='text-white font-bold mx-auto'>ILOŚĆ:</p>
-                            <input className='py-3 px-4 max-w-[4rem] text-center  rounded-lg  shadow-[6px_4px_8px_rgba(0,0,0,0.35)] md:mt-0  ' type="number" placeholder='2' />
+                            <p className='text-white font-bold mx-auto md:mx-0 '>WPISZ PRODUKT:</p>
+                            <input className='py-4 px-4 max-w-[18rem] rounded-lg  mx-auto shadow-[6px_4px_8px_rgba(0,0,0,0.35)]  ' type="text" placeholder='mleko...' />
                         </div>
 
-                        <div  >
-                            <p className='text-white font-bold mx-auto'>PODAJ CENĘ:</p>
-                            <input className='py-3 px-4 max-w-[6.5rem] text-left rounded-lg    shadow-[6px_4px_8px_rgba(0,0,0,0.35)] md:mt-0  ' type="number" placeholder='3.59' />
-                        </div>
-                    </div>
+                        <div className='flex justify-center gap-[4rem] mt-[2rem] md:mt-0'>
+                            <div >
+                                <p className='text-white font-bold mx-auto'>ILOŚĆ:</p>
+                                <input className='py-3 px-4 max-w-[4rem] text-center  rounded-lg  shadow-[6px_4px_8px_rgba(0,0,0,0.35)] md:mt-0  ' type="number" placeholder='2' />
+                            </div>
 
+                            <div  >
+                                <p className='text-white font-bold mx-auto'>PODAJ CENĘ:</p>
+                                <input className='py-3 px-4 max-w-[6.5rem] text-left rounded-lg    shadow-[6px_4px_8px_rgba(0,0,0,0.35)] md:mt-0  ' type="number" placeholder='3.59' />
+                            </div>
+                        </div>
+
+                    </div>
+                    <button className=' mt-[2rem] max-w-[400px] mx-auto bg-white shadow-[6px_4px_8px_rgba(0,0,0,0.50)] font-extrabold py-2 px-8 rounded-xl md:mt-[4rem]  '>DODAJ PRODUKT DO LISTY</button>
                 </div>
-                <button className=' mt-[2rem] max-w-[400px] mx-auto bg-white shadow-[6px_4px_8px_rgba(0,0,0,0.50)] font-extrabold py-2 px-8 rounded-xl md:mt-[4rem]  '>DODAJ PRODUKT DO LISTY</button>
+            </section>
 
 
-            </div>
 
+            <main className='mt-[8rem]'>
+
+
+
+
+                <img src={TwojaLista} alt="" />
+
+
+
+            </main>
 
 
         </div >
