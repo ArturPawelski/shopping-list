446385da273df3c98f6c112466adc7a86ae44c2a
ArturPawelski <arturpawelski13@gmail.com>
2023-03-20 17:13:17 +0100

    toggleColor

--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -29,6 +29,7 @@ const Lista = () => {
 
     const addProductToList = () => {
         const cena = cenaProduktu * iloscProduktu
+
         const newProduct = {
             nazwa: nazwaProduktu,
             ilosc: iloscProduktu,
@@ -40,7 +41,21 @@ const Lista = () => {
         setCenaProduktu(``);
     };
 
-    console.log(fullList)
+
+    const handleDelete = (index) => {
+        const newList = [...fullList];
+
+        newList.splice(index, 1);
+
+        setFullList(newList);
+    };
+
+
+    const [complete, setComplete] = useState(true)
+
+    const toggleComplete = () => {
+        setComplete(!complete)
+    }
 
 
     return (
@@ -98,14 +113,16 @@ const Lista = () => {
 
                         {fullList.map((product, index) => (
                             <section key={index} className='flex items-center justify-center gap-2 mt-[2rem] md:gap-8'>
-                                <BsTrash size={25} />
+
+                                <BsTrash size={25} onClick={() => handleDelete(index)} />
+
                                 <div className='flex justify-between items-center w-[90%] bg-[#B7C6FF] py-4 px-2 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] md:px-8'>
                                     <p className='font-bold '>{product.nazwa}</p>
 
                                     <div className='flex gap-4 items-center md:gap-16'>
                                         <p className='font-bold'>{product.ilosc}</p>
                                         <p className='font-bold'>{product.cena}</p>
-                                        <div className='bg-black max-w-[0.4rem] rounded-[100%] px-2 md:px-4'>.</div>
+                                        <div className={complete ? 'bg-black  max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4' : 'bg-red-600   max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4'} onClick={toggleComplete} ></div>
                                     </div>
                                 </div>
                             </section>
