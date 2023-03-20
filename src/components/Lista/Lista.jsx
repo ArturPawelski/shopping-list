4f408a9a9a29a9b855c1b33036c72274a6e0beeb
ArturPawelski <arturpawelski13@gmail.com>
2023-03-20 20:16:44 +0100

    component Twoja

--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { BiCaretDown } from 'react-icons/bi'
 import TwojaLista from '../images/TWOJA.png'
-import { BsTrash } from 'react-icons/bs'
+// import { BsTrash } from 'react-icons/bs'
+import Twoja from './Twoja'
 
 const Lista = () => {
 
@@ -117,24 +118,9 @@ const Lista = () => {
 
 
                     <div>
-
                         {fullList.map((product, index) => (
-                            <section key={index} className='flex items-center justify-center gap-2 mt-[2rem] md:gap-8'>
-
-                                <BsTrash size={25} onClick={() => handleDelete(index)} />
-
-                                <div className='flex justify-between items-center w-[90%] bg-[#B7C6FF] py-4 px-2 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] md:px-8'>
-                                    <p className='font-bold '>{product.nazwa}</p>
-
-                                    <div className='flex gap-4 items-center md:gap-16'>
-                                        <p className='font-bold'>{product.ilosc}</p>
-                                        <p className='font-bold'>{product.cena}</p>
-                                        <div className={product.status ? 'bg-black  max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4' : 'bg-red-600   max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4'} onClick={() => toggleComplete(index)} ></div>
-                                    </div>
-                                </div>
-                            </section>
+                            <Twoja product={product} index={index} handleDelete={handleDelete} toggleComplete={toggleComplete} />
                         ))}
-
 
                     </div>
 
@@ -155,3 +141,27 @@ const Lista = () => {
     )
 }
 export default Lista
+
+
+
+
+
+{/* <div>
+{fullList.map((product, index) => (
+    <section key={index} className='flex items-center justify-center gap-2 mt-[2rem] md:gap-8'>
+
+        <BsTrash size={25} onClick={() => handleDelete(index)} />
+
+        <div className='flex justify-between items-center w-[90%] bg-[#B7C6FF] py-4 px-2 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] md:px-8'>
+            <p className='font-bold '>{product.nazwa}</p>
+
+            <div className='flex gap-4 items-center md:gap-16'>
+                <p className='font-bold'>{product.ilosc}</p>
+                <p className='font-bold'>{product.cena}</p>
+                <div className={product.status ? 'bg-black  max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4' : 'bg-red-600   max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4'} onClick={() => toggleComplete(index)} ></div>
+            </div>
+        </div>
+    </section>
+))}
+
+</div> */}
---
+++ src/components/Lista/Twoja.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BsTrash } from 'react-icons/bs'
+
+
+const Twoja = ({ product, index, handleDelete, toggleComplete }) => {
+
+
+    return (
+
+        <section key={index} className='flex items-center justify-center gap-2 mt-[2rem] md:gap-8'>
+
+            <BsTrash size={25} onClick={() => handleDelete(index)} />
+
+            <div className='flex justify-between items-center w-[90%] bg-[#B7C6FF] py-4 px-2 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] md:px-8'>
+                <p className='font-bold '>{product.nazwa}</p>
+
+                <div className='flex gap-4 items-center md:gap-16'>
+                    <p className='font-bold'>{product.ilosc}</p>
+                    <p className='font-bold'>{product.cena}</p>
+                    <div className={product.status ? 'bg-black  max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4' : 'bg-red-600   max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4'} onClick={() => toggleComplete(index)} ></div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+    )
+}
+
+export default Twoja
