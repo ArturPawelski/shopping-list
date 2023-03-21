678fa4070e32eeede21911a448b97cf1c3e26d97
ArturPawelski <arturpawelski13@gmail.com>
2023-03-21 19:11:07 +0100

    end of the list layout

--- src/components/Lista/Lista.jsx
+++ src/components/Lista/Lista.jsx
@@ -112,13 +112,23 @@ const Lista = () => {
 
 
 
+
+
+
             <main className='mt-[8rem]'>
 
                 <img src={TwojaLista} alt="/" className='mx-auto w-auto px-8 md:max-w-[550px]' />
 
+
+
+
+
                 <div className='mt-[2rem] bg-white w-[90%] mx-auto rounded-2xl py-8 px-4 md:w-[85%] lg:w-[75%] xl:w-[70%] 2xl:w-[50%]'>
 
 
+                    <div className='text-right pr-3'>
+                        <button className='text-white bg-black px-4 py-2 tracking-[0.2rem] rounded-3xl  '>Filtruj</button>
+                    </div>
 
                     <div>
                         {fullList.map((product, index) => (
@@ -145,11 +155,9 @@ const Lista = () => {
                     </section>
 
 
-                    <section className=' relative flex items-center justify-center mt-[4rem] gap-[2rem] left-[1.8rem]'>
-                        <button className='bg-[#4A55AA] text-white font-extrabold px-8 py-4 text-center rounded-[25px] text-[1.5rem] tracking-[0.3rem]'>ZAPISZ LISTĘ</button>
+                    <section className=' relative flex items-center justify-center mt-[4rem] gap-[0.5rem] left-[1.8rem] sm:gap-[2rem] '>
+                        <button className='bg-[#4A55AA] text-white font-extrabold px-2 py-4 text-center rounded-[25px] text-[1rem] tracking-[0.3rem] sm:text-[1.5rem] sm:px-4'>ZAPISZ LISTĘ</button>
                         <BsTrash size={35} />
-
-
 
 
                     </section>
--- src/components/Lista/Twoja.jsx
+++ src/components/Lista/Twoja.jsx
@@ -14,13 +14,17 @@ const Twoja = ({ product, index, handleDelete, toggleComplete }) => {
 
             <BsTrash size={25} onClick={() => handleDelete(index)} />
 
-            <div className='flex justify-between items-center w-[90%] bg-[#B7C6FF] py-4 px-2 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] md:px-8'>
-                <p className='font-bold '>{product.nazwa}</p>
+            <div className='flex justify-between items-center w-[90%] bg-[#B7C6FF] py-3 px-4 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] md:px-8'>
+                <p className='font-bold'>{product.nazwa}</p>
 
-                <div className='flex gap-4 items-center md:gap-16'>
+                <div className='flex gap-4 items-center md:gap-16 '>
                     <p className='font-bold'>{product.ilosc}</p>
                     <p className='font-bold'>{product.cena}</p>
-                    <div className={product.status ? 'bg-black  max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4' : 'bg-red-600   max-w-[0.3rem] min-h-[1.5rem] rounded-[100%] px-2 md:px-4'} onClick={() => toggleComplete(index)} ></div>
+
+                    <div className='max-w-[1rem] ' onClick={() => toggleComplete(index)} >
+                        {product.status ? <BsCircle size={25} /> : <AiOutlineCheckCircle size={25} />}
+
+                    </div>
                 </div>
             </div>
         </section>
