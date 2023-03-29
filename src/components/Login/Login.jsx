0886aad078c6703074417ef376f9e75130ad355a
ArturPawelski <arturpawelski13@gmail.com>
2023-03-29 18:26:05 +0200

    login.jsx colors

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,31 +2,31 @@ import React from 'react'
 import BgLogin from '../images/signup-bg.png'
 const Login = () => {
     return (
-        <div className='w-full font-rob min-h-screen flex flex-col justify-center items-center bg-black '>
+        <div className='w-full font-rob min-h-screen flex flex-col justify-center items-center bg-black bg-opacity-60 '>
 
 
 
 
-            <div className='flex gap-[4rem]'>
-
-                <main className='bg-white min-w-[400px] min-h-[600px] relative px-8 py-8 overflow-hidden  '>
-                    <img src={BgLogin} alt="" className=' absolute  top-[-30%]   left-[0%] ' />
 
 
-                    <section className=' '>
+            <main className='bg-white min-w-[400px] min-h-[600px] relative px-8 py-8 overflow-hidden rounded-2xl shadow-2xl '>
+                <img src={BgLogin} alt="" className=' absolute  top-[-30%]   left-[0%] ' />
 
 
-                        <p className=''>email</p>
+                <section className=' '>
 
 
-                        <input type="text" className='bg-[#D9D9D9] w-full' />
-                    </section>
-
-                </main>
+                    <p className=''>email</p>
 
 
+                    <input type="text" className='bg-[#D9D9D9] w-full' />
+                </section>
 
-            </div>
+            </main>
+
+
+
+
 
 
         </div >
