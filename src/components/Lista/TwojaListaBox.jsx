963d24000f810b1ee43328f406e3412c35131f37
ArturPawelski <arturpawelski13@gmail.com>
2023-04-11 20:49:03 +0200

    editEnabled changes

--- src/components/Lista/TwojaListaBox.jsx
+++ src/components/Lista/TwojaListaBox.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsTrash } from 'react-icons/bs'
 
 import { BsCircle } from 'react-icons/bs'
@@ -12,30 +12,33 @@ import { AiOutlineCheckCircle } from 'react-icons/ai'
 const TwojaListaBox = ({ product, index, handleDelete, toggleComplete, setFullList }) => {
 
 
-    const [editEnabled, setEditEnabled] = React.useState(false)
+    const [editEnabled, setEditEnabled] = useState(false)
 
 
-    function editName(event) {
-        console.log(event)
+    function editName() {
         setEditEnabled(true)
     }
-    function saveName(event) {
-        console.log()
+    function saveName() {
         setEditEnabled(false)
     }
 
 
     function setName(event) {
-        console.log(event)
+
         const newName = event.target.value
         setFullList(previousList => {
             let newList = Array.from(previousList) // możesz spróbować znaleźć odpowiedź dlaczego
-            console.log(newList)
+
             newList[index].nazwa = newName
-            console.log(newList)
+
             return newList
         })
     }
+
+
+
+
+
 
 
     return (
@@ -45,10 +48,15 @@ const TwojaListaBox = ({ product, index, handleDelete, toggleComplete, setFullLi
             <BsTrash size={25} onClick={() => handleDelete(index)} />
 
             <div className='flex flex-col justify-between items-center w-[90%] bg-[#B7C6FF] py-3 px-4 rounded-xl shadow-[0px_5px_4px_rgba(0,0,0,0.35)] sm:flex-row md:px-8'>
-                {editEnabled || <p className='font-bold'>{product.nazwa}</p>}
-                {editEnabled && <input value={product.nazwa} onChange={setName}></input>}
-                {editEnabled || <button onClick={editName}> edit</button>}
-                {editEnabled && <button onClick={saveName}>save</button>}
+
+
+                {!editEnabled ? <p className='font-bold'>{product.nazwa}</p> : <input value={product.nazwa} onChange={setName}></input>}
+
+
+                {!editEnabled ? <button onClick={editName}> edit</button> : <button onClick={saveName}>save</button>}
+
+
+
 
                 <div className='flex gap-4 items-center py-2 sm:py-0 md:gap-16 '>
                     <p className='font-bold'>{product.ilosc}</p>
